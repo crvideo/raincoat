f928361a47f10ef0195499501d4ae0e1688ef62a
Rui Chen <crotoc@gmail.com>
2022-04-10 14:56:00 +0000

    update Readme; generate dist for pip;update logs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from subprocess import check_call
 from setuptools.command.install import install
 
-APP_NAME = "Raincoat"
+APP_NAME = "raincoat_prowlarr"
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
@@ -14,18 +14,26 @@ class PostInstallCommand(install):
         if not os.path.exists(cfg_path):            
             cfg = open(cfg_path, 'x')
             j = dict(
-            jackett_apikey="",
-            jackett_url="http://127.0.0.1:9117",
-            jackett_indexer="all",
-            description_length=100,
-            exclude="",
-            results_limit=20,
-            client_url="",
-            display="grid",
-            torrent_client="transmission",
-            torrent_client_username="",
-            torrent_client_password="",
-            download_dir=""
+                indexer_manager="jackett",
+                jackett_apikey="",
+                jackett_url="http://127.0.0.1:9117",
+                jackett_indexer="all",
+                prowlarr_apikey="",
+                prowlarr_url="http://127.0.0.1:9117",
+                prowlarr_indexer="",
+                description_length=100,
+                exclude="",
+                results_limit=20,
+                client_url="",
+                display="grid",
+                torrent_client="transmission",
+                torrent_client_username="",
+                torrent_client_password="",
+                download_dir="",
+                nzbget_url= "http://127.0.0.1",
+                nzbget_username= "",
+                nzbget_password= "",
+                nzbget_port= 6789
             )
             json.dump(j, cfg, indent=4)
         install.run(self)
@@ -34,27 +42,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="raincoat-jackett",
-    version="1.3",
-    author="Gabisonfire",
-    author_email="gabisonfire@github.com",
-    description="Raincoat is a tool to search torrents using Jackett and send them to your client.",
-    keywords="transmission qbittorrent deluge jackett torrent",
+    name="raincoat-prowlarr",
+    version="0.2.1",
+    author="crvideoVR",
+    author_email="crvideoVR@outlook.com",
+    description="Raincoat-prowlarr is a tool to search torrents using jackett and search torrents and nzb files using prowlarr and send them to your client.",
+    keywords="transmission qbittorrent deluge jackett torrent nzbget",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
     packages=setuptools.find_packages(),
     entry_points = {
-        "console_scripts": ['raincoat = raincoat.raincoat:main']
+        "console_scripts": ['raincoat_prowlarr = raincoat_prowlarr.raincoat_prowlarr:main']
         },
-    url="https://github.com/Gabisonfire/raincoat",
+    url="https://github.com/crvideo/raincoat_prowlarr.git",
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
         "Topic :: Communications :: File Sharing",
     ],
-    install_requires=["requests==2.23.0", "justlog", "colorama==0.4.3", "tabulate==0.8.7", "transmission-clutch==6.0.2", "deluge-client==1.8.0", "python-qbittorrent==0.4.2"],
+    install_requires=["requests>=2.23.0", "justlog", "colorama==0.4.3", "tabulate==0.8.7", "transmission-clutch==6.0.2", "deluge-client==1.8.0", "python-qbittorrent==0.4.2"],
     cmdclass={
         'install': PostInstallCommand
     },
